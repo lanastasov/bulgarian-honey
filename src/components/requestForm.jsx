bdcaa6ee5b5f809d43857c6e0b5e6b02269c59d2
lanastasov <lubomir.anastasov@gmail.com>
2021-12-29 11:18:29 +0200

    hmlFor and handleSubmit

--- src/components/requestForm.jsx
+++ src/components/requestForm.jsx
@@ -2,8 +2,14 @@ import React, { Component } from "react";
 
 class RequestForm extends Component {
   state = {
-    data: { username: "", password: "" },
+    data: { name: "", password: "" },
     errors: {},
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Call the server
+    console.log("submitted");
   };
 
   render() {
@@ -12,36 +18,36 @@ class RequestForm extends Component {
     return (
       <section className="container-section">
         <div className="elementor-container">
-          <form className="row g-3">
-            <h2 class="elementor-heading-title elementor-size-default">
+          <form className="row g-3" onSubmit={this.handleSubmit}>
+            <h2 className="elementor-heading-title elementor-size-default">
               Request a quotation
             </h2>
             <div className="col-md-6">
-              <label for="inputName" className="form-label">
+              <label htmlFor="inputName" className="form-label">
                 Name<b>*</b>
               </label>
               <input type="text" className="form-control" id="inputName" />
             </div>
             <div className="col-md-6">
-              <label for="inputCompany4" className="form-label">
+              <label htmlFor="inputCompany4" className="form-label">
                 Company
               </label>
               <input type="text" className="form-control" id="inputCompany4" />
             </div>
             <div className="col-md-6">
-              <label for="inputPhone" className="form-label">
+              <label htmlFor="inputPhone" className="form-label">
                 Phone<b>*</b>
               </label>
               <input type="tel" className="form-control" id="inputPhone" />
             </div>
             <div className="col-md-6">
-              <label for="inputEmail4" className="form-label">
+              <label htmlFor="inputEmail4" className="form-label">
                 Email<b>*</b>
               </label>
               <input type="email" className="form-control" id="inputEmail4" />
             </div>
             <div className="col-md-12">
-              <label for="inlineCheckbox1" className="form-label">
+              <label htmlFor="inlineCheckbox1" className="form-label">
                 Sort of interest
               </label>
               <br />
@@ -52,7 +58,7 @@ class RequestForm extends Component {
                   id="inlineCheckbox1"
                   value="option1"
                 />
-                <label className="form-check-label" for="inlineCheckbox1">
+                <label className="form-check-label" htmlFor="inlineCheckbox1">
                   Acacia
                 </label>
               </div>
@@ -63,7 +69,7 @@ class RequestForm extends Component {
                   id="inlineCheckbox2"
                   value="option2"
                 />
-                <label className="form-check-label" for="inlineCheckbox2">
+                <label className="form-check-label" htmlFor="inlineCheckbox2">
                   Polyflora
                 </label>
               </div>
@@ -74,13 +80,13 @@ class RequestForm extends Component {
                   id="inlineCheckbox3"
                   value="option3"
                 />
-                <label className="form-check-label" for="inlineCheckbox3">
+                <label className="form-check-label" htmlFor="inlineCheckbox3">
                   Linden
                 </label>
               </div>
             </div>
             <div className="col-md-6">
-              <label for="inputQuantity" className="form-label">
+              <label htmlFor="inputQuantity" className="form-label">
                 Quantity
                 <br />
                 <small>in MT (metric tons) or KG (please specify)</small>
@@ -88,7 +94,7 @@ class RequestForm extends Component {
               <input type="text" className="form-control" id="inputQuantity" />
             </div>
             <div className="col-md-6">
-              <label for="gridRadios1" className="form-label">
+              <label htmlFor="gridRadios1" className="form-label">
                 Conventional or Organic Honey
               </label>
               <div className="col-sm-10">
@@ -99,9 +105,8 @@ class RequestForm extends Component {
                     name="gridRadios"
                     id="gridRadios1"
                     value="option1"
-                    checked
                   />
-                  <label className="form-check-label" for="gridRadios1">
+                  <label className="form-check-label" htmlFor="gridRadios1">
                     Conventional
                   </label>
                 </div>
@@ -113,7 +118,7 @@ class RequestForm extends Component {
                     id="gridRadios2"
                     value="option2"
                   />
-                  <label className="form-check-label" for="gridRadios2">
+                  <label className="form-check-label" htmlFor="gridRadios2">
                     Organic
                   </label>
                 </div>
@@ -121,7 +126,10 @@ class RequestForm extends Component {
             </div>
 
             <div className="mb-3">
-              <label for="exampleFormControlTextarea1" className="form-label">
+              <label
+                htmlFor="exampleFormControlTextarea1"
+                className="form-label"
+              >
                 Message
               </label>
               <textarea
@@ -133,7 +141,7 @@ class RequestForm extends Component {
 
             <div className="col-12">
               <div className="form-check">
-                <label className="form-check-label" for="gridCheck">
+                <label className="form-check-label" htmlFor="gridCheck">
                   GDPR Acceptance
                   <b>*</b>
                 </label>
@@ -143,7 +151,7 @@ class RequestForm extends Component {
                   type="checkbox"
                   id="gridCheck"
                 />
-                <label className="form-check-label" for="gridCheck">
+                <label className="form-check-label" htmlFor="gridCheck">
                   <small>
                     I agree that this site maintains the information sent so
                     that they can respond to my request
